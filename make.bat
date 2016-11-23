d1febdd8d00eafa74f66d94e1be06d7619133070
Andrew Davison <andrew.davison@gmail.com>
2016-11-23 14:36:12 +1000

    No longer support Windows development related to Issue #5, no great loss

--- make.bat
+++
@@ -1,3 +0,0 @@
-set USE_SSL64=0
-if "%Platform%" == "x64" set USE_SSL64=1
-nmake USE_SSL=0 USE_SSL64=%USE_SSL64% -f WINmakefile
--- makeiso.bat
+++
@@ -1,3 +0,0 @@
-set USE_SSL64=0
-if "%Platform%" == "x64" set USE_SSL64=1
-nmake ISO_ONLY=1 USE_SSL=0 -f WINmakefile
