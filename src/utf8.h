c6db813d35c7b37a75a725216e1329511f7d40bc
Andrew Davison <andrew.davison@gmail.com>
2016-12-13 07:46:01 +1000

    More for Issue #44

--- src/utf8.h
+++ src/utf8.h
@@ -1,11 +1,15 @@
 #ifndef UTF8_H
 #define UTF8_H
 
-extern size_t strlen_utf8(const char *s);
-extern int get_char_utf8(const char **src);
-extern int put_char_utf8(char *dst, int ch);
-extern int isalpha_utf8(int ch);
+// These relate to similar stdc functions...
 
 extern int getc_utf8(FILE *fp);
+extern size_t strlen_utf8(const char *s);
+extern int isalpha_utf8(int ch);
+
+// These just get/put a memory buffer...
+
+extern int get_char_utf8(const char **src);
+extern int put_char_utf8(char *dst, int ch);
 
 #endif
