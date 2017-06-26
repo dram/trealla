90f4b68d96f7b145b4eecc13ef87bdc7fa3d3273
Andrew Davison <andrew.davison@gmail.com>
2017-06-26 14:00:23 +1000

    Work on issue #181

--- src/trealla.c
+++ src/trealla.c
@@ -1567,11 +1567,11 @@ trealla *trealla_create(const char *name)
 
 #ifndef ISO_ONLY
 #define RANDOM_STR __DATE__
-	trealla_make_rule(pl, "recorda(K,V) :- recorda(K,V,_).");
+	trealla_make_rule(pl, "recorda(K,V) :- nonvar(K), nonvar(V), asserta('$" RANDOM_STR "'(K,V),R).");
 	trealla_make_rule(pl, "recorda(K,V,R) :- nonvar(K), nonvar(V), asserta('$" RANDOM_STR "'(K,V),R).");
-	trealla_make_rule(pl, "recordz(K,V) :- recordz(K,V,_).");
+	trealla_make_rule(pl, "recordz(K,V) :- nonvar(K), nonvar(V), assertz('$" RANDOM_STR "'(K,V),R).");
 	trealla_make_rule(pl, "recordz(K,V,R) :- nonvar(K), nonvar(V), assertz('$" RANDOM_STR "'(K,V),R).");
-	trealla_make_rule(pl, "recorded(K,V) :- recorded(K,V,_).");
+	trealla_make_rule(pl, "recorded(K,V) :- clause('$" RANDOM_STR "'(K,V),_,_).");
 	trealla_make_rule(pl, "recorded(K,V,R) :- clause('$" RANDOM_STR "'(K,V),_,R).");
 	trealla_make_rule(pl, "current_key(K) :- '$" RANDOM_STR "'(K,_).");
 	trealla_make_rule(pl, "instance(R,V) :- nonvar(R), clause('$" RANDOM_STR "'(_,V),_,R).");
