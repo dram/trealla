1e3c6850e9466e08a62204f37a449cc70f571e6b
Andrew Davison <andrew.davison@gmail.com>
2017-07-02 15:13:10 +1000

    Misc

--- src/network.c
+++ src/network.c
@@ -111,11 +111,6 @@
 #define POLLRDHUP 0
 #endif
 
-#ifdef OPENSSL_NO_TLS1_METHOD
-#define TLS_client_method TLSv1_client_method
-#define TLS_method TLSv1_method
-#endif
-
 #define DEFAULT_CIPHERS "HIGH:!aNULL" // EECDH+AESGCM:EDH+AESGCM:EECDH+AES256:EDH+AES256
 
 static const int g_debug = 0;
