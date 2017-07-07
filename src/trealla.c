43f5bde93f353b6bc0a5c908718017354a8109ac
Andrew Davison <andrew.davison@gmail.com>
2017-07-07 18:54:18 +1000

    Misc

--- src/trealla.c
+++ src/trealla.c
@@ -1573,10 +1573,10 @@ trealla *trealla_create(const char *name)
 	trealla_make_rule(pl, "once(G) :- call(G), !.");
 
 	trealla_make_rule(pl, "not(G) :- call(G), !, fail.");
-	trealla_make_rule(pl, "not(G) :- true.");
+	trealla_make_rule(pl, "not(_G) :- true.");
 
 	trealla_make_rule(pl, "\\+ G :- call(G), !, fail.");
-	trealla_make_rule(pl, "\\+ G :- true.");
+	trealla_make_rule(pl, "\\+ _G :- true.");
 
 #ifndef ISO_ONLY
 	trealla_make_rule(pl, "recorda(K,V) :- nonvar(K), nonvar(V), asserta(?SYSTEMSTR(K,V),_).");
